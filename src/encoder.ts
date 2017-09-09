968062464d16c4d77c9c91380326e18676b0ae39
Yota Toyama <raviqqe@gmail.com>
2017-09-09 19:23:32 +0900

    Modify encoder

--- src/encoder.ts
+++ src/encoder.ts
@@ -1,5 +1,18 @@
+import Buffer from "./buffer";
+
 export default class {
+    private buffer: Buffer = new Buffer();
+
     public encode(x: any): Uint8Array {
-        return undefined;
+        return this.buffer.toArray();
+    }
+
+    private encodeAny(x: any): void {
+        switch (typeof x) {
+            case "number":
+                throw new Error(`Encoder for ${x} is not implemented yet.`);
+            default:
+                throw new Error(`Encoder for ${x} is not implemented yet.`);
+        }
     }
 }
